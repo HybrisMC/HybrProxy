bf17442971af1e100c6ad296cf57cac00ca7dc09
TBHGodPro <tarun.bhaskar@outlook.com>
2023-06-14 10:29:11 -0500

    fix jsonc reading

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -63,7 +63,9 @@ export async function readConfig(): Promise<Config> {
   const data = await readFile(filePath, 'utf8');
 
   try {
-    const parsed = JSON.parse(data);
+    const parsed = JSON.parse(
+      data.replace(/\/\/.*|\/\*\*(.|\n|\r|\r\n|\n\r)*\*\//g, '')
+    );
     if (validate(parsed, configSchema).valid) {
       return parsed;
     } else {
@@ -79,7 +81,9 @@ export function readConfigSync(): Config {
   const data = readFileSync(filePath, 'utf8');
 
   try {
-    const parsed = JSON.parse(data);
+    const parsed = JSON.parse(
+      data.replace(/\/\/.*|\/\*\*(.|\n|\r|\r\n|\n\r)*\*\//g, '')
+    );
     if (validate(parsed, configSchema).valid) {
       return parsed;
     } else {
