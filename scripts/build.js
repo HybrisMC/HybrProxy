5313513933e1968de0924e25b3b2e9da9ce35a19
TBHGodPro <tarun.bhaskar@outlook.com>
2023-06-15 19:38:41 -0500

    Add node version warnings in dashboard building

--- scripts/build.js
+++ scripts/build.js
@@ -12,7 +12,18 @@ run('npx', ['tsc']);
 
 if (!process.argv.includes('--noDashboard')) {
   console.log('info: building dashboard 🌐');
-  run('cd dashboard && npm run build');
+
+  const nodeVersion = parseInt(process.version.split('.')[0].substring(1));
+
+  if (nodeVersion < 16)
+    throw new Error('You must use Node 16 or higher to build dashboard!');
+  if (nodeVersion > 16) console.log('fix: Setting OpenSSL Provider 🐛');
+
+  run(
+    nodeVersion > 16
+      ? 'cd dashboard && NODE_OPTIONS=--openssl-legacy-provider npm run build'
+      : 'cd dashboard && npm run build'
+  );
 }
 
 console.log('\ninfo: build successful! 🎉\n -> Run the app using `npm start`');
