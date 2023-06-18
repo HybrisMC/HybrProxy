6076c30ef09cc0c8a2aaa3210550223778b6ca85
thororen <78185467+thororen1234@users.noreply.github.com>
2023-06-17 20:45:42 -0400

    Update build.js
    Possible fix for OS related node options
    
    Signed-off-by: thororen <78185467+thororen1234@users.noreply.github.com>

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+import { platform } from 'node:process';
 
 function run(executable, params = []) {
   execSync(`${executable} ${params.join(' ')}`, {
@@ -14,16 +15,31 @@ if (!process.argv.includes('--noDashboard')) {
   console.log('info: building dashboard 🌐');
 
   const nodeVersion = parseInt(process.version.split('.')[0].substring(1));
-
+  
   if (nodeVersion < 16)
     throw new Error('You must use Node 16 or higher to build dashboard!');
   if (nodeVersion > 16) console.log('fix: Setting OpenSSL Provider 🐛');
 
-  run(
+  var isWin = process.platform === "win32";
+  if (isWin === true) run(
     nodeVersion > 16
-      ? 'cd dashboard && NODE_OPTIONS=--openssl-legacy-provider npm run build'
+      ? 'cd dashboard && set NODE_OPTIONS=--openssl-legacy-provider && npm run build'
       : 'cd dashboard && npm run build'
   );
+  var isMac = process.platform === "darwin";
+  if (isMac === true) run(
+    nodeVedsion > 16
+      ? 'cd dashboard && export NODE_OPTIONS=--openssl-legacy-provider && npm run build'
+      : 'cd dashboard && npm run build'
+  );
+  var isLinux = process.platform === "linux";
+  if (isLinux === true) run(
+    nodeVedsion > 16
+      ? 'cd dashboard && export NODE_OPTIONS=--openssl-legacy-provider && npm run build'
+      : 'cd dashboard && npm run build'
+  );
+
+
 }
 
 console.log('\ninfo: build successful! 🎉\n -> Run the app using `npm start`');
