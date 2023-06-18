f15940f0302985f23b7b861e7a6fdb77c61474fd
thororen <78185467+thororen1234@users.noreply.github.com>
2023-06-18 14:30:52 -0400

    Update build.js
    Signed-off-by: thororen <78185467+thororen1234@users.noreply.github.com>

--- scripts/build.js
+++ scripts/build.js
@@ -29,7 +29,11 @@ if (!process.argv.includes('--noDashboard')) {
       ? 'cd dashboard && NODE_OPTIONS=--openssl-legacy-provider npm run build'
       : 'cd dashboard && npm run build'
   );
-  // do your thing for linux skull
+  if (process.platform === "linux") run(
+    nodeVersion > 16
+      ? 'cd dashboard && NODE_OPTIONS=--openssl-legacy-provider npm run build'
+      : 'cd dashboard && npm run build'
+  );
 }
 
 console.log('\ninfo: build successful! 🎉\n -> Run the app using `npm start`');
